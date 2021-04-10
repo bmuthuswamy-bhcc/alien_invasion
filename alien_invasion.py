6acf83572ad4efa34051c002f5b5a01cd5958fde
bharathwaj <b.muthuswamy@bhcc.edu>
2021-04-10 14:21:16 -0400

    Added movement to fleet of aliens
    Fleet of aliens move right initially, collide with the right edge, then
    move down and in the opposite direction (towards the left edge).
    Movemenet continues.

--- alien_invasion.py
+++ alien_invasion.py
@@ -47,6 +47,7 @@ class AlienInvasion:
 				if bullet.rect.bottom <= 0:
 					self.bullets.remove(bullet)			
 
+			self._update_aliens()
 			self._update_screen()
 
 	def _check_events(self):
@@ -111,6 +112,24 @@ class AlienInvasion:
 		alien.rect.y = alien.y
 
 		self.aliens.add(alien)
+
+	def _check_fleet_edges(self):
+		"""Respond appropriately if any aliens have reached an edge."""
+		for alien in self.aliens.sprites():
+			if alien.check_edges():
+				self._change_fleet_direction()
+				break
+
+	def _change_fleet_direction(self):
+		"""Drop the entire fleet and change the fleet's direction."""
+		for alien in self.aliens.sprites():
+			alien.rect.y += self.settings.fleet_drop_speed
+		self.settings.fleet_direction *= -1
+
+	def _update_aliens(self):
+		"""Update the positions of all aliens in the fleet."""
+		self._check_fleet_edges()
+		self.aliens.update()
 
 	def _update_screen(self):
 		"""Update images on the screen, and flip to the new screen."""
